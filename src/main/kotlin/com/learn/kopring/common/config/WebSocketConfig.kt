96b97530134cbb3d2bf40cd0a46f877c19addf5c
SoohoLee <hyeri0603@naver.com>
2024-02-16 22:57:43 +0900

    refactor: 시간 계산 로직 수정

--- src/main/kotlin/com/learn/kopring/common/config/WebSocketConfig.kt
+++ src/main/kotlin/com/learn/kopring/common/config/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package com.learn.kopring.common.config
 
+import com.learn.kopring.websocket.handler.StompHandler
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -11,11 +13,18 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/pt").setAllowedOrigins("*").withSockJS() // 배포 후 cors 설정을 변경
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("*")
+            .withSockJS()
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("*")
+    }
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/sub")            // 서버가 클라이언트로 브로커 주소
+        registry.setApplicationDestinationPrefixes("/pub")  // 클라이언트가 서버로 메시지를 보낼 때 사용할 주소
     }
 
-    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/practice")            // 서버가 클라이언트로 브로커 주소
-        registry.setApplicationDestinationPrefixes("/client")  // 클라이언트가 서버로 메시지를 보낼 때 사용할 주소
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(StompHandler())
     }
 }
